d769c76963fed894a830f0f3bef597a94af0722e
CodeSlogan <mrchen40@163.com>
2021-11-18 12:25:45 +0800

    dataresource commit

---
+++ src/main/java/com/codeslogan/config/DruidConfig.java
@@ -0,0 +1,56 @@
+package com.codeslogan.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: codeslogan
+ * @Date: 2021-11-18 10:33
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    //后台监控
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+
+        Map<String, String> initParameters = new HashMap<>();
+
+        //增加配置
+        initParameters.put("loginUsername", "admin");
+        initParameters.put("loginPassword", "123456");
+
+        initParameters.put("allow", "");
+
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> initParameters = new HashMap<>();
+        //不统计
+        initParameters.put("exclusions", "*.js,*.css,/druid/*");
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+}
