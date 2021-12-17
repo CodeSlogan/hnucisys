381cabbb0a7f4f94a07068b0a4da4451d4e1faba
CodeSlogan <mrchen40@163.com>
2021-12-17 17:41:41 +0800

    add url in detail

--- src/main/java/com/codeslogan/controller/DetailController.java
+++ src/main/java/com/codeslogan/controller/DetailController.java
@@ -1,6 +1,8 @@
 package com.codeslogan.controller;
 
+import com.codeslogan.pojo.Competition;
 import com.codeslogan.pojo.CptContent;
+import com.codeslogan.service.CompetitionService;
 import com.codeslogan.service.CptContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,9 +22,14 @@ public class DetailController {
     @Autowired
     CptContentService cptContentService;
 
+    @Autowired
+    CompetitionService competitionService;
+
     @RequestMapping("/detail/{cptId}")
     public String showCptContent(@PathVariable int cptId, Model model) {
         CptContent cptContent = cptContentService.queryCptContentById(cptId);
+        Competition competition = competitionService.getCompetitionById(cptId);
+        model.addAttribute("cpt", competition);
         model.addAttribute("cptContent", cptContent);
         return "detail";
     }
