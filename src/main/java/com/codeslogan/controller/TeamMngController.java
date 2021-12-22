996e170b5c51b13c41de817923db07ed2bcba2c1
CodeSlogan <mrchen40@163.com>
2021-12-22 15:34:55 +0800

    modify newteam

--- src/main/java/com/codeslogan/controller/TeamMngController.java
+++ src/main/java/com/codeslogan/controller/TeamMngController.java
@@ -1,26 +1,29 @@
 package com.codeslogan.controller;
 
-import com.codeslogan.pojo.Competition;
-import com.codeslogan.pojo.CptContent;
-import com.codeslogan.pojo.Team;
-import com.codeslogan.pojo.TeamExhibition;
+import com.codeslogan.pojo.*;
 import com.codeslogan.service.CompetitionService;
 import com.codeslogan.service.CptContentService;
 import com.codeslogan.service.TeamService;
+import com.codeslogan.service.TeamUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class TeamMngController {
     private String message;
     @Autowired
     TeamService teamService;
+
+    @Autowired
+    TeamUserService teamUserService;
 
     @Autowired
     CompetitionService competitionService;
@@ -40,14 +43,25 @@ public class TeamMngController {
     }
 
     @RequestMapping("/submitNewTeam")
-    public String submitNewTeam(Team team, Model model) {
+    public String submitNewTeam(Team team, Model model, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+
         Date date = new Date();
         team.setCreateTime(date);
         team.setUpdateTime(date);
         Competition competition = competitionService.getCompetitionById(team.getCptId());
         System.out.println(competition);
+        TeamUser teamUser = new TeamUser();
+        teamUser.setUserId(user.getUserId());
+        teamUser.setRole(2);
+        teamUser.setCreateTime(date);
+        teamUser.setUpdateTime(date);
         if (competition != null) {
             teamService.createNewTeam(team);
+            List<Team> teams = teamService.queryTeamDesc();
+            Team team1 = teams.get(0);
+            teamUser.setTeamId(team1.getTeamId());
+            teamUserService.save(teamUser);
             message = "提示：队伍创建成功！";
         } else {
             message = "错误：竞赛编号不存在，请查看竞赛后确认！";
